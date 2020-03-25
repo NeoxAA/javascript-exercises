9762742f05d8d5f361699f9c37fc422f0be8de33
Asam <aainvests@outlook.com>
2020-03-24 20:22:48 -0400

    finished reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+function reverseString(theString) {
+    splitString = theString.split("");
+    reverseIt = splitString.reverse();
+    joinIt = reverseIt.join("");
+    return joinIt
 }
 
 module.exports = reverseString
